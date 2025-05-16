45ea9379d467acded14f77465ab65053806b179d
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 15:08:59 +0530

    Add BART.SP.OCR.CP.Web/BART.SP.OCR.CP.Web/BART.SP.OCR.CP.Web/MenuPage1/MenuPage1UserControl.ascx.cs 1747388337812

---
+++ BART.SP.OCR.CP.Web/BART.SP.OCR.CP.Web/BART.SP.OCR.CP.Web/MenuPage1/MenuPage1UserControl.ascx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace BART.SP.OCR.CP.Web.MenuPage1
+{
+    public partial class MenuPage1UserControl : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
