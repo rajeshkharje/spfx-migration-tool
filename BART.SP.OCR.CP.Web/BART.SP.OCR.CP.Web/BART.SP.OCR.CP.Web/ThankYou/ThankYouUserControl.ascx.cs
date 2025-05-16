a1c12d8c8a749b4b508318b215f37f43b25eb410
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 15:05:57 +0530

    Add BART.SP.OCR.CP.Web/BART.SP.OCR.CP.Web/BART.SP.OCR.CP.Web/ThankYou/ThankYouUserControl.ascx.cs 1747388156973

---
+++ BART.SP.OCR.CP.Web/BART.SP.OCR.CP.Web/BART.SP.OCR.CP.Web/ThankYou/ThankYouUserControl.ascx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace BART.SP.OCR.CP.Web.ThankYou
+{
+    public partial class ThankYouUserControl : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
